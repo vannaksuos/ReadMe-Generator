78215152b736706d6aa5e5224ff3eac2e3d9cf0c
Vannak <vannaksuos@gmail.com>
2020-05-11 10:39:10 -0400

    trying to pull

--- index.js
+++ index.js
@@ -4,98 +4,100 @@ const inquirer = require("inquirer");
 const markDown = require("./generateMarkdown");
 
 inquirer
-    .prompt([
-    {
-        message: "Enter your GitHub username:",
-        name: "username"
-    }, 
-    {
-        message: "Whats your link to your deploy github",
-        name:"deploy"
-    },
-    { 
-        message: "What is the Title?",
-        name: "title"
-    },
-    {
-        message: "What do you want in your badges?",
-        name: "badges"
-    },
-    {
-        message: "Can you describe your project?",
-        name: "description"
-    },
-    {
-        message: "List each section of your table of contents, separated by a comma",
-        name: "contents",
-        
-    },
-    {
-        message: "How do you install it?",
-        name:"installation"
-    },
-    {
-        message: "How do you use it?",
-        name:"usage"
-    },
-    {
-        message: "Can you give us license info?",
-        name:"license"
-    },
-    {
-        message: "Any Contribution?",
-        name:"contributing"
-    },
-    {
-        message:"How did you test it?",
-        name: "test"
-    },
-    {
-        message:"If you have any questions for me, please email me at",
-        name:"questions"     
-    },
-    
-    {
-        message: "Do you have an Gif Demo",
-        name: "Gif"
-    },
+    .prompt([{
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            message: "Whats your link to your deploy github",
+            name: "deploy"
+        },
+        {
+            message: "What is the Title?",
+            name: "title"
+        },
+        {
+            message: "What do you want in your badges?",
+            name: "badges"
+        },
+        {
+            message: "Can you describe your project?",
+            name: "description"
+        },
+        {
+            message: "List each section of your table of contents, separated by a comma",
+            name: "contents",
+
+        },
+        {
+            message: "How do you install it?",
+            name: "installation"
+        },
+        {
+            message: "How do you use it?",
+            name: "usage"
+        },
+        {
+            message: "Can you give us license info?",
+            name: "license"
+        },
+        {
+            message: "Any Contribution?",
+            name: "contributing"
+        },
+        {
+            message: "How did you test it?",
+            name: "test"
+        },
+        {
+            message: "If you have any questions for me, please email me at",
+            name: "questions"
+        },
+
+        {
+            message: "Do you have an Gif Demo",
+            name: "Gif"
+        },
 
 
-])
-  .then(function(answers) {
-    const queryUrl = `https://api.github.com/users/${answers.username}`;
+    ])
+    .then(function (answers) {
+        const queryUrl = `https://api.github.com/users/${answers.username}`;
 
-    axios.get(queryUrl).then(function(res) {  
-        const proImage = res.data.avatar_url
-        const email = res.data.email
-    
-        answers.proImage = proImage;
-        answers.email = email;
-        let updateStr = "<br/>"
+        axios.get(queryUrl).then(function (res) {
+            const proImage = res.data.avatar_url
+            const email = res.data.email
 
-        let tableContent =answers.contents
-        let tableContentArray = tableContent.split(",") 
-        for(let i = 0; i < tableContentArray.length; i++) {
-            let str = `${i + 1}. ${tableContentArray[i].trim()}<br/>`
-            updateStr += str
-        }
+            answers.proImage = proImage;
+            answers.email = email;
+            let updateStr = "<br/>"
+
+            let tableContent = answers.contents
+            let tableContentArray = tableContent.split(",")
+            for (let i = 0; i < tableContentArray.length; i++) {
+                let str = `${i + 1}. ${tableContentArray[i].trim()}<br/>`
+                updateStr += str
+            }
             answers.contents = updateStr
 
-        let markDownText = markDown(answers)
-        console.log(answers)
-        fs.writeFile("readme.md", markDownText, function(err) {
-            if (err) {
-              throw err;
-            }
-    
-          });
-        return {proImage, email};
-        
-    })
-    
+            let markDownText = markDown(answers)
+            console.log(answers)
+            fs.writeFile("readme.md", markDownText, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+            });
+            return {
+                proImage,
+                email
+            };
+
+        })
+
     });
-    module.exports = {
-        add: function (a, b) {
-            return a + b;
-        }
-    };
+module.exports = {
+    add: function (a, b) {
+        return a + b;
+    }
+};
--- readme.md
+++ readme.md
@@ -1,33 +1,32 @@
 
 # ![profile pic for school](https://avatars1.githubusercontent.com/u/59664686?v=4)
 
-# Email:Vannaksuos@gmail.com
+# Email:(null)
 
 # Deploy Github link
 
-<https://github.com/vannaksuos/ReadMe-Generator>
+https://vannaksuos.github.io/burger/ 
 
-# Title: Readme Generator
+# Title:Burger
 
 # Badges
 
-[![Build Status](https://travis-ci.com/vannaksuos/ReadMe-Generator.svg?branch=master)](https://travis-ci.com/vannaksuos/ReadMe-Generator)
 
 # Description
 
-When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
+ This assignment, Ive create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). I'm following the MVC design pattern; use Node and MySQL to query and route data in my app, and Handlebars to generate my HTML.
 
 # Table of contents
 
-<br/>1. Title<br/>2. Description<br/>3. Table of Contents<br/>4. Installation<br/>5. Usage<br/>6. License<br/>7. Contribution<br/>8. Tests<br/>9. Questions<br/>10. Demo<br/>
+<br/>1. Titile<br/>2. Description<br/>3. Table of Contents<br/>4. Installation<br/>5. Usage<br/>6. License<br/>7. Contribution<br/>8. Test<br/>9. Question<br/>10. Demo<br/>
 
 # Installation
 
-npm install
+npm init <br> npm i mysql<br> npm i express
 
 # Usage
 
-to generate a readme.md file for github
+run node server.js
 
 # License
 
@@ -35,32 +34,21 @@ MIT License
 
 Copyright (c) 2020 Vannak Suos
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # Contribution
 
+
 # Test
 
-travis cl
+Travis ci
 
-# If you have questions for me you can email me at
+# If you have any question you like to ask me you can email me at
 
-vannaksuos@gmail.com
+Vannaksuos@gmail.com
 
 # Demo
